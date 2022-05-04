4a69706eaca68f6524a998a4aecaab55cc60f693
Manex Lete <104761531+manexlete@users.noreply.github.com>
2022-05-04 07:54:23 +0000

    Día 3 de programación

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-variable = 10
+# import keyword
 
-print(variable)
+# print(keyword)
+# print(keyword.kwlist)
 
-def funcion():
-  x=5
-  return x
+# variable = 10
 
-# Este es un comentario de prueba
+# print(variable)
 
-print (funcion())
+# def funcion():
+#   x=5
+#   return x
 
-nombre = "Manex"
+# # Este es un comentario de prueba
 
-print ("Hola", nombre)
+# print (funcion())
+
+# nombre = "Manex"
+
+# print ("Hola", nombre)
+
+# print ("Esto es todo", end=" ")
+# print("amigos!")
+
+#tipos = "Hola mundo!"
+#print("Identidad:",id(tipos))
+
+total_decimales = 158000000
+print(total_decimales.replace(",","."))
